156fad60e7fb6755171e4eb98a0f571c1d1dbf0c
khant-min <kmin01405@gmail.com>
2023-11-20 21:03:13 +0630

    feat: apply header & footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,5 +1,29 @@
 import React from "react";
 
 export default function Footer() {
-  return <div>Footer</div>;
+  const creator = "Khant Min";
+  const today = new Date().getFullYear();
+  return (
+    <div style={{ width: "100%", backgroundColor: "#89DA00" }}>
+      <h2
+        style={{
+          color: "#F7F2EE",
+          padding: "0.5rem 1rem",
+          textAlign: "center",
+          fontSize: "20px",
+          fontWeight: "semibold",
+          display: "flex",
+          justifyContent: "space-between",
+        }}
+      >
+        <span>&copy;{today} All Rights Reserved.</span>
+        <span>
+          Powered by{" "}
+          <a href="https://github.com/khantmin" target="blank">
+            {creator}
+          </a>
+        </span>
+      </h2>
+    </div>
+  );
 }
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from "react";
 
 export default function Header() {
   return (
-    <div>Header</div>
-  )
+    <div style={{ width: "100%", backgroundColor: "#89DA00" }}>
+      <h1
+        style={{
+          color: "#F7F2EE",
+          padding: "0.25rem",
+          textAlign: "center",
+          fontSize: "40px",
+          fontWeight: "semibold",
+        }}
+      >
+        Magic Library
+      </h1>
+    </div>
+  );
 }
