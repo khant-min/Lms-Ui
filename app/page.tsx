5e5a0cf44235a6f4f5d2d0a91acd65dd5ce60753
khant-min <kmin01405@gmail.com>
2023-11-24 22:43:04 +0630

    feat: apply nav

--- app/page.tsx
+++ app/page.tsx
@@ -2,5 +2,18 @@ import React from "react";
 import Link from "next/link";
 
 export default function page() {
-  return <div>home</div>;
+  return (
+    <div>
+      <div>
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/create-books">Create Books</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
